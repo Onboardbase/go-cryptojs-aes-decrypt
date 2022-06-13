abfc0c6e7da4ebc9db14a1ae8826c7135dd2aa11
Aleem Isiaka <aleemisiaka@gmail.com>
2022-06-13 08:03:15 +0100

    Push

---
+++ example/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	aesdecrypt "github.com/limistah/go-cryptojs-aes-decrypt/decrypt"
+)
+
+func main () {
+	cipher := "U2FsdGVkX1/cJw+ISoZKbICgFmoJ+Ehx/atOTgPlt4IOCpmgrHJBY9J2gvoupPbNQs3dHsshX2EljZPlnVLATRJRyF8rOMui+uFMl8DV9I0VVA+1IqecvcP+a2gswGSjYEOiEG9kKQQ8cNlZPtGZdD4ksxxKxm/DhDxquzYX9mWwtMLe0EVYqFd0gncHX2wYzOrvKbMLZlGn/rN70XC+kQgEd47syLFOBm/cjzmVFGvymJfvuRNDl66PRzMtoGqZkQ7uufZuFS3rRUpcsiVv+0XbkSTiaZeD2t7cs+pn4G4uAymMJathk2z2pmhhd184rnXpuooaz8pXPmfdL8CwbQ=="
+    // The passphrase that generated the cipher above
+    passphrase := "passcode"
+	decrypted := aesdecrypt.Run(cipher, passphrase)
+	fmt.Println(decrypted)
+	// Outputs -> '{"id":"0a09a9e1-eee2-4a8e-bf77-901be943621c","key":"WHAT","value":"IS_THIS_NAH","comment":"","addedBy":{"name":"Aleem Isiaka","id":"a3748a40-3ab8-4dd1-bac6-e73c1047c95f"},"addedDate":"April 22, 2022 | 20:09 GMT","method":""}'
+}
